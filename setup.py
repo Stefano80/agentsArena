954c787c6022a40015eb00adf6c712f3aac9d65a
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-07-01 17:01:57 +0200

    Create initial framework for a python package.

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+import agentsArena
+
+setuptools.setup(
+    name="agentsArena",
+    license="GPL3",
+    keywords="agentsArena chess tuner",
+    url="https://github.com/Stefano80/agentsArena",
+    test_suite="test",
+)
